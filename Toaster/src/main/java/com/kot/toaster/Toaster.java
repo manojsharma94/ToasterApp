2f3d86fc340543464119682bd0672a0a0c717446
Manoj Sharma <msharma@anystr.com>
2022-11-17 14:22:50 +0530

    Initial commit

---
+++ Toaster/src/main/java/com/kot/toaster/Toaster.java
@@ -0,0 +1,11 @@
+package com.kot.toaster;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+    public static void Toasting(Context context,String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
+
+    }
+}
---
+++ app/src/main/java/com/kot/toasterapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kot.toasterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
