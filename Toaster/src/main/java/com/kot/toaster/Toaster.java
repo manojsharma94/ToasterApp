f822c7bbca1edacf66d77f8ae9c1bf0b5b57afa8
Manoj Sharma <msharma@anystr.com>
2022-11-17 16:09:25 +0530

    second commit

--- Toaster/src/main/java/com/kot/toaster/Toaster.java
+++ Toaster/src/main/java/com/kot/toaster/Toaster.java
@@ -1,11 +1,26 @@
 package com.kot.toaster;
 
 import android.content.Context;
+import android.location.Location;
 import android.widget.Toast;
+
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesWithFallbackProvider;
 
 public class Toaster {
     public static void Toasting(Context context,String msg){
-        Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
 
+
+        SmartLocation.with(context)
+                .location(new LocationGooglePlayServicesWithFallbackProvider(context))
+                .oneFix()
+                .start(new OnLocationUpdatedListener() {
+                    @Override
+                    public void onLocationUpdated(Location location) {
+                        Toast.makeText(context,String.valueOf(location.getLatitude())+ String.valueOf(location.getLongitude()),Toast.LENGTH_LONG).show();
+                       // onLocationFetchFinished.onFinished(String.valueOf(location.getLatitude()), String.valueOf(location.getLongitude()));
+                    }
+                });
     }
 }
